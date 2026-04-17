b14638ad341af70b30268e626043653cb174ab48
Leonardo Vieira <142953306+avLeoo@users.noreply.github.com>
2026-04-16 21:50:34 -0300

    Add files via upload

---
+++ leonardo/marisa2/js/login.js
@@ -0,0 +1,6 @@
+function enviarDadosLogin(){
+    var login = document.getElementById("log_id").value;
+    var senha = document.getElementById("senha_id").value;
+
+    alert(login + " | " + senha)
+}
